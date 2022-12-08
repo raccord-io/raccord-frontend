f102de2f29815b35416edfff1e80f2d2cf038aae
Maxime Carabina <maxime.carabina@epitech.eu>
2022-12-08 13:31:59 +0100

    ci(colors): added new colors

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -6,28 +6,28 @@ const themeConfig = {
       black: '#000',
       white: '#fff'
     },
-    primary: '#1890ff',
+    primary: '#738DA7',
     success: green[6],
     error: red[5],
     pink: '#ff1890',
     green: green,
     red: red,
-    background: '#f0f2f5',
-    divider: 'rgba(0, 0, 0, 0.12)',
-    grey: ['#f0f2f5', '#d8d9dc', '#c0c1c4'],
-    orange: ['#ff5f1f', '#fa541c', '#ff8718'],
-    blue: [
-      '#e6f7ff',
-      '#bae7ff',
-      '#91d5ff',
-      '#69c0ff',
-      '#40a9ff',
-      '#1890ff',
-      '#096dd9',
-      '#0050b3',
-      '#003a8c',
-      '#002766'
-    ]
+    background: '#E9E9E9',
+    // divider: 'rgba(0, 0, 0, 0.12)',
+    // grey: ['#f0f2f5', '#d8d9dc', '#c0c1c4'],
+    // orange: ['#ff5f1f', '#fa541c', '#ff8718'],
+    // blue: [
+    //   '#e6f7ff',
+    //   '#bae7ff',
+    //   '#91d5ff',
+    //   '#69c0ff',
+    //   '#40a9ff',
+    //   '#1890ff',
+    //   '#096dd9',
+    //   '#0050b3',
+    //   '#003a8c',
+    //   '#002766'
+    // ]
   },
   spacingUnit: 8
 };
