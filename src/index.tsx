6107a24226f0bec9cdf1ca164d2c952a244666cb
Maxime Carabina <maxime.carabina@epitech.eu>
2022-12-08 13:31:26 +0100

    feat(router): added react router for managing pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
