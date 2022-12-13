049bbe8715b3ff8c2ba8aecb8c8362249473b121
Victor PALLE <victor.palle@epitech.eu>
2022-12-13 17:20:24 +0100

    feat(highlight): all highlight working

--- src/pages/Project/Project.tsx
+++ src/pages/Project/Project.tsx
@@ -4,6 +4,7 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { useProjectQuery } from '../../services/projectApi';
 
 import { SequencesCollapse } from '../../components';
+import { ScriptContainer } from '../../components/ScriptContainer/ScriptContainer';
 
 const text =
   'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
@@ -22,11 +23,7 @@ function Project() {
       <div className="columns">
         <div className="script-pages-previews"></div>
         <div className="script-container">
-          <div className="script-page">
-            <div className="script-text-container">
-              <p>{text}</p>
-            </div>
-          </div>
+          <ScriptContainer content={text} />
         </div>
         <div className="collapse">
           <SequencesCollapse />
